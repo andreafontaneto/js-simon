ad7c4b3ea5cc2e6f7e8ec607017ea9eab2f48c58
andreafontaneto <andrea.fontaneto@gmail.com>
2021-11-09 17:49:38 +0100

    create array + for refix

--- js/script.js
+++ js/script.js
@@ -11,12 +11,25 @@ Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei num
 // intercetto il div dove stampare l'output
 const numbersBox = document.querySelector('.numbers');
 
-// salvo la funzione che genera 5 numeri casuali in una variabile
-let generatedRandomNumbers = getRandomNumbers();
-// console.log(generatedRandomNumbers);
+numbersBox.innerHTML //variabile che richiama la funzione che genera i 5 numeri casuali
 
-numbersBox.innerHTML = //funzione che genera i 5 numeri casuali
+// creo un array vuoto dove andranno i 5 numeri estratti
+const arrayNumbers = [];
 
+// ciclo per 5 volte per generare 5 numeri casuali
+const nTot = 5;
+
+for(let i=0; i < nTot; i++){
+
+  // salvo in una variabile la funzione che genera un numero casuale
+  let randomNumbers = generateRandomNumber(1,100);
+ 
+  // pusho i numeri estratti nell'array vuoto
+  arrayNumbers.push(randomNumbers);
+
+}
+
+console.log(arrayNumbers);
 
 
 /*--------------------
@@ -27,21 +40,6 @@ FUNCTIONS
 function generateRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 };
-
-// salvo il ciclo for in una funzione
-function getRandomNumbers(){
-
-  // ciclo la funzione che genera un numero casuale (generateRandomNumber) per 5(nTot) volte per crere 5 numeri casuali
-  const nTot = 5;
-  
-  for(let i=0; i < nTot; i++){
-  
-    // salvo in una variabile la funzione che genera un numero casuale per nTot volte
-    let randomNumbers = generateRandomNumber(1,100);
-    console.log(randomNumbers);
-  }
-
-}
 
 
 
