e5bd66bf7a95c743a461275508b05e0bcbfc62e6
andreafontaneto <andrea.fontaneto@gmail.com>
2021-11-09 18:03:10 +0100

    print array numbers

--- js/script.js
+++ js/script.js
@@ -11,8 +11,6 @@ Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei num
 // intercetto il div dove stampare l'output
 const numbersBox = document.querySelector('.numbers');
 
-numbersBox.innerHTML //variabile che richiama la funzione che genera i 5 numeri casuali
-
 // creo un array vuoto dove andranno i 5 numeri estratti
 const arrayNumbers = [];
 
@@ -20,17 +18,18 @@ const arrayNumbers = [];
 const nTot = 5;
 
 for(let i=0; i < nTot; i++){
-
+  
   // salvo in una variabile la funzione che genera un numero casuale
   let randomNumbers = generateRandomNumber(1,100);
- 
+  
   // pusho i numeri estratti nell'array vuoto
   arrayNumbers.push(randomNumbers);
-
+  
 }
 
 console.log(arrayNumbers);
 
+numbersBox.innerHTML = `${arrayNumbers}`;
 
 /*--------------------
 FUNCTIONS
@@ -40,6 +39,3 @@ FUNCTIONS
 function generateRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 };
-
-
-
